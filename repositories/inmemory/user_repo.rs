c5d9d3f63aeeb1621edbdbe87c17db238f59049a
NgoveniGift <139237390+NgoveniGift@users.noreply.github.com>
2025-04-24 16:17:17 +0200

    Update user_repo.rs

--- repositories/inmemory/user_repo.rs
+++ repositories/inmemory/user_repo.rs
@@ -1,1 +1,36 @@
 
+use std::collections::HashMap;
+use crate::repository_interface::{Repository, User, UserRepository};
+
+pub struct InMemoryUserRepository {
+    store: HashMap<String, User>,
+}
+
+impl InMemoryUserRepository {
+    pub fn new() -> Self {
+        Self {
+            store: HashMap::new(),
+        }
+    }
+}
+
+impl Repository<String, User> for InMemoryUserRepository {
+    fn save(&mut self, id: String, entity: User) {
+        self.store.insert(id, entity);
+    }
+
+    fn find_by_id(&self, id: &String) -> Option<&User> {
+        self.store.get(id)
+    }
+
+    fn find_all(&self) -> Vec<&User> {
+        self.store.values().collect()
+    }
+
+    fn delete(&mut self, id: &String) {
+        self.store.remove(id);
+    }
+}
+
+impl UserRepository for InMemoryUserRepository {}
+
