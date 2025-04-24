3ad4bac295a7964a058f37808fc2e807bbe444fd
NgoveniGift <139237390+NgoveniGift@users.noreply.github.com>
2025-04-24 16:20:31 +0200

    Update weather_repo.rs

--- repositories/inmemory/weather_repo.rs
+++ repositories/inmemory/weather_repo.rs
@@ -1,1 +1,36 @@
 
+use std::collections::HashMap;
+use crate::repository_interface::{Repository, WeatherReport, WeatherReportRepository};
+
+pub struct InMemoryWeatherReportRepository {
+    store: HashMap<String, WeatherReport>,
+}
+
+impl InMemoryWeatherReportRepository {
+    pub fn new() -> Self {
+        Self {
+            store: HashMap::new(),
+        }
+    }
+}
+
+impl Repository<String, WeatherReport> for InMemoryWeatherReportRepository {
+    fn save(&mut self, id: String, entity: WeatherReport) {
+        self.store.insert(id, entity);
+    }
+
+    fn find_by_id(&self, id: &String) -> Option<&WeatherReport> {
+        self.store.get(id)
+    }
+
+    fn find_all(&self) -> Vec<&WeatherReport> {
+        self.store.values().collect()
+    }
+
+    fn delete(&mut self, id: &String) {
+        self.store.remove(id);
+    }
+}
+
+impl WeatherReportRepository for InMemoryWeatherReportRepository {}
+
