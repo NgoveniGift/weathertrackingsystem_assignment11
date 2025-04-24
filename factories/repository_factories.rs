fb7fa0f1930829ee87ca20f1f68d0bb8ee427401
NgoveniGift <139237390+NgoveniGift@users.noreply.github.com>
2025-04-24 16:44:00 +0200

    Update repository_factories.rs

--- factories/repository_factories.rs
+++ factories/repository_factories.rs
@@ -1,1 +1,26 @@
 
+use crate::repository_interface::*;
+use crate::repositories::inmemory::{user_repo::InMemoryUserRepository, weather_repo::InMemoryWeatherReportRepository};
+
+pub enum StorageType {
+    InMemory,
+    FileSystem, // Stub for future support
+}
+
+pub struct RepositoryFactory;
+
+impl RepositoryFactory {
+    pub fn get_user_repository(storage: StorageType) -> Box<dyn UserRepository> {
+        match storage {
+            StorageType::InMemory => Box::new(InMemoryUserRepository::new()),
+            StorageType::FileSystem => unimplemented!("FileSystemUserRepository not yet implemented"),
+        }
+    }
+
+    pub fn get_weather_report_repository(storage: StorageType) -> Box<dyn WeatherReportRepository> {
+        match storage {
+            StorageType::InMemory => Box::new(InMemoryWeatherReportRepository::new()),
+            StorageType::FileSystem => unimplemented!("FileSystemWeatherReportRepository not yet implemented"),
+        }
+    }
+}
